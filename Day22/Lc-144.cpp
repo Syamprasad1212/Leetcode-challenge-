380ca6af65ed23518080c3e6473a29e8c5964cab
Syamprasad1212 <syamofficial1212@gmail.com>
2025-09-10 18:53:38 +0530

    Create Lc-144.cpp

---
+++ Day22/Lc-144.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>res;
+        preorder(root,res);
+        return res;
+    }
+    void preorder(TreeNode* node, vector<int>&res)
+    {
+        if(!node)return;
+        res.push_back(node->val);
+        preorder(node->left,res);
+        preorder(node->right,res);
+    }
+};
