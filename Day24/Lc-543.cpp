3be321d3dd6bb9c76b6b6fc268c9995df58449cb
Syamprasad1212 <syamofficial1212@gmail.com>
2025-09-12 18:10:47 +0530

    Create Lc-543.cpp
    Dfs

---
+++ Day24/Lc-543.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        dfs(root, diameter);
+        return diameter;
+    }
+
+    int dfs(TreeNode* node, int& diameter) {
+        if (!node) return 0;
+
+        int left = dfs(node->left, diameter);
+        int right = dfs(node->right, diameter);
+
+        diameter = max(diameter, left + right);
+
+        return 1 + max(left, right);
+    }
+};
